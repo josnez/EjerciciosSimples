26bbe46723f2f3616ab1f79831219350e84a595d
Josue Nuñez Prada <37277791+josnow5@users.noreply.github.com>
2020-02-25 09:49:56 -0500

    Update Ejercicio19.py

--- Ejercicio19.py
+++ Ejercicio19.py
@@ -2,23 +2,24 @@ def main():
     h = int(input("Digite las horas: \n"))
     m = int(input("Digite los minutos: \n"))
     s = int(input("Digite los segundos: \n"))
+    auxH = 0
+    auxM = 0
+    auxS = 0
 
-    imprimir(h, m, s)
-    while(h!=0 or m!=0 or s!=0):
-        if s > 0 and s < 61:
-            s-=1
-            imprimir(h, m, s)
+    while(auxH!=h or auxM!=m or auxS!=s):
+        if auxS<61:
+            auxS+=1
+            imprimir(auxH, auxM, auxS)
         else:
-            s=60
-            if m > 0 and m < 61:
-                m -= 1
-                imprimir(h, m, s)
+            auxS=0
+            if(auxM<61):
+                auxM+=1
+                imprimir(auxH, auxM, auxS)
             else:
-                m=60
-                if h>0 and h<20:
-                    h-=1
-                    imprimir(h, m, s)
-
+                auxM=0
+                if auxH<20:
+                    auxH+=1
+                    imprimir(auxH, auxM, auxS)
 
 def imprimir(h, m, s):
     print("Horas: "+str(h) + " Minutos: " + str(m) + " Segundos: " + str(s))
